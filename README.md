2a3c79eeb7463c1832a842a7a9db2b75f9bea4a5
Ryan Ciecalone <ryan@ryanchristianmedia.com>
2015-08-11 15:41:20 -0600

    Updated code in main.js and index.html, now able to add game title, genre, and year to DOM and library

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # JavaScript Game Library
-
 ## Part 1
 
-1. Create a `Game()` class with the following properties:
+<!-- 1. Create a `Game()` class with the following properties:
   - `title` - title of the game
   - `genre` - the game's genre
 
-1. Create a `render()` method on the `Game()` prototype for adding instances of the `Game()` class to the DOM.
+1. Create a `render()` method on the `Game()` prototype for adding instances of the `Game()` class to the DOM. -->
 
-1. Create a `GameLibrary()` class with the following properties:
+<!-- 1. Create a `GameLibrary()` class with the following properties:
   -  `title` - title of the game library - i.e., 'Video Games', 'Board Games', etc.
-  -  `games` - array of all `Game()` instances
+  -  `games` - array of all `Game()` instances -->
 
 1. Create a `render()` method on the `GameLibrary()` prototype for adding instances of the `GameLibrary()` class to the DOM.
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,87 @@
-// add scripts
+  // console.log('sanity check!');
+
+function Game(title, genre, year){
+  this.title = title;
+  this.genre = genre;
+  this.year = year;
+}
+
+
+Game.prototype.render = function(game){
+  for(var name in game) {
+  $('#games').append(game[name], ' ');
+    }
+    $('#games').append('<br>');
+};
+
+function GameLibrary(title, games){
+    this.title = title;
+    this.games = [];
+}
+
+GameLibrary.prototype.renderLibrary = function(){
+  for(var i =0; i < this.games.length; i ++) {
+   $('#gameLibrary').append(this.games[i].title + ' ' + this.games[i].genre + ' ' +this.games[i].year + '<br>');
+console.log(this.games[i]);
+  }
+};
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
-});
+
+zeldaLibrary = new GameLibrary('Zelda Games');
+
+
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    $('#gameLibrary').html('');
+    var game1 = new Game($('.title').val(), $('.genre').val(), $('.year').val());
+    zeldaLibrary.games.push(game1);
+    zeldaLibrary.renderLibrary();
+console.log(zeldaLibrary);
+  });
+
+
+
+
+});//documentready
+
+
+
+
+
+
+
+
+
+
+
+
+
+// zeldaLibrary.games.push(zelda1);
+// zeldaLibrary.games.push(zelda2);
+// zeldaLibrary.games.push(zelda3);
+// zeldaLibrary.games.push(zelda4);
+// zeldaLibrary.renderLibrary();
+// console.log(zeldaLibrary);
+
+
+// zelda1 = new Game('The Legend of Zelda', 'adventure', 1986);
+// zelda2 = new Game('Zelda II, the adventure of link', 'adventure', 1988);
+// zelda3 = new Game('A Link to the past', 'adventure', 1988);
+// zelda4 = new Game('Links Awakening', 'adventure', 1993);
+// zeldaLibrary = new GameLibrary('Zelda Games');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
